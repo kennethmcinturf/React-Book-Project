ca49d417619fb9b80d73a03437819227dc4f94f6
kennethmcinturf <43353352+kennethmcinturf@users.noreply.github.com>
2020-08-17 15:28:37 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,81 @@
+import React from "react";
+import "./styles.css";
+
+const App = () => {
+  const handleChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const stories = [
+    {
+      title: "React",
+      url: "https://reactjs.org/",
+      author: "Jordan Walke",
+      num_comments: 3,
+      points: 4,
+      objectID: 0
+    },
+    {
+      title: "Redux",
+      url: "https://redux.js.org/",
+      author: "Dan Abramov, Andrew Clark",
+      num_comments: 2,
+      points: 5,
+      objectID: 1
+    }
+  ];
+
+  const handleSearch = (event) => {
+    console.log(event.target.value);
+  };
+
+  return (
+    <div className="App">
+      <h1>My Hacker Stories</h1>
+
+      <Search onSearch={handleSearch} />
+
+      <hr />
+
+      <List list={stories} />
+    </div>
+  );
+};
+
+const Search = (props) => {
+  const [searchTerm, setSearchTerm] = React.useState("");
+
+  const handleChange = (event) => {
+    setSearchTerm(event.target.value);
+
+    props.onSearch(event);
+  };
+
+  return (
+    <div>
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" onChange={handleChange}></input>
+
+      <p>
+        Searching for <strong>{searchTerm}</strong>.
+      </p>
+    </div>
+  );
+};
+
+const List = (props) => {
+  return props.list.map(function (item) {
+    return (
+      <div key={item.objectID}>
+        <span>
+          <a href={item.url}>{item.title}</a>
+        </span>
+        <span>{item.author}</span>
+        <span>{item.num_comments}</span>
+        <span>{item.points}</span>
+      </div>
+    );
+  });
+};
+
+export default App;
